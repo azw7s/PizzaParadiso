5631466a80debe56c9191bb0f4390b7eecaf4012
azw7s <amirhossein.sf313@gmail.com>
2024-05-31 10:33:33 +0300

    new host

--- aa_pizza/local_settings.py
+++ aa_pizza/local_settings.py
@@ -1,6 +1,7 @@
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
+
 ALLOWED_HOSTS = ['pizzaparadiso-87c4c4557382.herokuapp.com', '127.0.0.1', 'www.pizzaparadiso-kw.com']
 
 # Database
