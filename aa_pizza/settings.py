16dbbb0816cdab1713cb0ea97682bc75facd7de5
azw7s <amirhossein.sf313@gmail.com>
2024-06-06 10:51:18 +0300

    media_url modified

--- aa_pizza/settings.py
+++ aa_pizza/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
@@ -170,6 +170,8 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = os.getenv('AWS_S3_FILE_OVERWRITE')
 AWS_DEFAULT_ACL = os.getenv('AWS_DEFAULT_ACL', None)
 DEFAULT_FILE_STORAGE = os.getenv('DEFAULT_FILE_STORAGE')
+
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
 
 # Stripe
 
