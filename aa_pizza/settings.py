dee43cafc5cd0b9085af7cac28274d9105b8f648
amir hossein <154253664+azw7s@users.noreply.github.com>
2024-11-13 08:56:35 +0300

    Update settings.py

--- aa_pizza/settings.py
+++ aa_pizza/settings.py
@@ -172,6 +172,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = os.environ.get('AWS_S3_FILE_OVERWRITE')
 AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL', None)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+print("==========================================", AWS_SECRET_ACCESS_KEY, "============================================")
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
